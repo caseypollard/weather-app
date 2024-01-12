d4d23e2c198ecf7bb93386e051f7583d48aed863
caseypollard <cjpollard0407@hotmail.com>
2024-01-12 20:15:17 +1100

    Added basic JS to search form and h1

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function handleSearchForm(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("h1");
+    cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchForm);
